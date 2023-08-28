fefeeba37ed0912551aa83cbb000d52d987ed1ef
Srini Pandiyan <125408728+srinipandiyan@users.noreply.github.com>
2023-08-28 15:36:21 -0500

    Update models.py

--- models.py
+++ models.py
@@ -94,20 +94,22 @@ class User(db.Model):
         nullable=False,
     )
 
-    messages = db.relationship('Message')
+    messages = db.relationship('Message', back_populates="user")
 
     followers = db.relationship(
         "User",
         secondary="follows",
         primaryjoin=(Follows.user_being_followed_id == id),
-        secondaryjoin=(Follows.user_following_id == id)
+        secondaryjoin=(Follows.user_following_id == id),
+        back_populates="following"
     )
 
     following = db.relationship(
         "User",
         secondary="follows",
         primaryjoin=(Follows.user_following_id == id),
-        secondaryjoin=(Follows.user_being_followed_id == id)
+        secondaryjoin=(Follows.user_being_followed_id == id),
+        back_populates="followers"
     )
 
     likes = db.relationship(
@@ -197,7 +199,7 @@ class Message(db.Model):
         nullable=False,
     )
 
-    user = db.relationship('User')
+    user = db.relationship('User', back_populates="messages")
 
 
 def connect_db(app):
