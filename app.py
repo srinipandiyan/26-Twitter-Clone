316badd89d8215ac9bb0a367a0f79d803f984d59
Srini Pandiyan <125408728+srinipandiyan@users.noreply.github.com>
2023-08-30 20:37:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 # Use shell command to turn on debug mode.
 # $ export FLASK_DEBUG=1 && flask run
